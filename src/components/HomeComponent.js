d4c67e3db736320b2c6771be4158dccc72ea5f2b
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-09-04 13:46:42 +0800

    added (Main) HomeComponent Component

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react'
 import Jumbotron from './Jumbotron'
 import Features from './Features';
 import FeaturedItems from './FeaturedItems';
+import Navbar from '../Navbar'
 class HomeComponent extends Component{
     render(){
         return(
