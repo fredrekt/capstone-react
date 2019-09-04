f4595d1049c1484336d2f8ca468171d85853c0bf
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-09-04 13:44:53 +0800

    added switch,browserRouter & route

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React from 'react';
 import Navbar from './Navbar';
 import {BrowserRouter, Switch ,Route} from 'react-router-dom'
 import Footer from './Footer'
-import HomeComponent from './components/HomeComponent';
+import HomeComponent from './components/HomeComponent'
+import AboutComponent from './components/AboutComponent'
+import MedicinesComponent from './components/MedicinesComponent'
+import ContactComponent from './components/ContactComponent'
+import About from './components/About'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      {/* <Switch>
-        <Route path="/" component={}/>
-        <Route path="/about" component={}/>
-        <Route path="/medicines" component={}/>
-        <Route path="/contact" component={}/>
-      </Switch> */}
-      <HomeComponent/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+          <Switch>
+            <Route path="/" exact component={HomeComponent}/>
+            <Route path="/about" component={AboutComponent}/>
+            <Route path="/contact" component={ContactComponent}/>
+          </Switch>
+        <Footer/>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
