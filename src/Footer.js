554e3f329718bddb7dbeb71eff16d58d9c065240
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-09-08 22:53:51 +0800

    footer component updated

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const Footer = () => {
   return (
-    <MDBFooter color="black" className="text-center font-small pb-4 pt-4 mt-4">
+    <MDBFooter color="black" style={{'padding':'200px'}} className="footer-size text-center font-small pb-4 pt-4 mt-4">
       
         <MDBContainer fluid>
           &copy; {new Date().getFullYear()} Copyright | <a href="#fredgaringo"> Fred Garingo </a>
