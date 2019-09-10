a0f1ab51a9907d9b6f6776f8ab52e11b6f5e90d0
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-09-10 21:18:40 +0800

    added MDBAnimation & react-reveal (onScroll) for ContactComponents (Contact, Company)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,79 +1,86 @@
 import React from "react";
 import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon, MDBBtn, MDBInput } from "mdbreact";
 import phone from '../img/phone.png'
+import { MDBAnimation } from "mdbreact"
+import Slide from 'react-reveal/Slide'
+
 const Contact = () => {
   return (
-    <section className="container margin-fix">
-      <MDBCard>
-        <MDBRow>
-          <MDBCol lg="8">
-            <MDBCardBody className="form">
-              <h1 className="c-header">
-                Get in Touch
-              </h1>
-              <MDBRow>
-                <MDBCol md="6">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="text"
-                      id="form-contact-name"
-                      label="Your name"
-                    />
-                  </div>
-                </MDBCol>
-                <MDBCol md="6">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="text"
-                      id="form-contact-email"
-                      label="Your email"
-                    />
-                  </div>
-                </MDBCol>
-              </MDBRow>
-              <MDBRow>
-                <MDBCol md="6">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="text"
-                      id="form-contact-phone"
-                      label="Your phone"
-                    />
-                  </div>
-                </MDBCol>
-                <MDBCol md="6">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="text"
-                      id="form-contact-company"
-                      label="Your company"
-                    />
-                  </div>
-                </MDBCol>
-              </MDBRow>
-              <MDBRow>
-                <MDBCol md="12">
-                  <div className="md-form mb-0">
-                    <MDBInput
-                      type="textarea"
-                      id="form-contact-message"
-                      label="Your message"
-                    />
-                    <MDBBtn rounded color="blue">
-                      <MDBIcon icon="paper-plane" />
-                    </MDBBtn>
-                  </div>
-                </MDBCol>
-              </MDBRow>
-            </MDBCardBody>
-          </MDBCol>
-          <MDBCol lg="4">
-              <hr className="hr-light my-4" />
-              <img className="phone-size" src={phone}/>
-          </MDBCol>
-        </MDBRow>
-      </MDBCard>
-    </section>
+    <Slide up>
+      <section className="container margin-fix">
+        <MDBCard>
+          <MDBRow>
+            <MDBCol lg="8">
+              <MDBCardBody className="form">
+                <MDBAnimation type="flash delay-3s">
+                <h1 className="c-header">
+                  Get in Touch
+                </h1>
+                </MDBAnimation>
+                <MDBRow>
+                  <MDBCol md="6">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="text"
+                        id="form-contact-name"
+                        label="Your name"
+                      />
+                    </div>
+                  </MDBCol>
+                  <MDBCol md="6">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="text"
+                        id="form-contact-email"
+                        label="Your email"
+                      />
+                    </div>
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                  <MDBCol md="6">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="text"
+                        id="form-contact-phone"
+                        label="Your phone"
+                      />
+                    </div>
+                  </MDBCol>
+                  <MDBCol md="6">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="text"
+                        id="form-contact-company"
+                        label="Your company"
+                      />
+                    </div>
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                  <MDBCol md="12">
+                    <div className="md-form mb-0">
+                      <MDBInput
+                        type="textarea"
+                        id="form-contact-message"
+                        label="Your message"
+                      />
+                      <MDBBtn rounded color="blue">
+                        <MDBIcon icon="paper-plane" />
+                      </MDBBtn>
+                    </div>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCol>
+            <MDBCol lg="4">
+                <hr className="hr-light my-4" />
+                <img className="phone-size" src={phone}/>
+            </MDBCol>
+          </MDBRow>
+        </MDBCard>
+      </section>
+    </Slide>
   );
 }
 
