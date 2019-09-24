64b177d39d3060851d76feb6bd33f203f0b3e6ad
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-09-25 00:11:03 +0800

    made allMeds (Route) to be The OTC Section (qry for mysql db)

--- Routes/allMeds.js
+++ Routes/allMeds.js
@@ -6,7 +6,6 @@ Router.get("/",(req, res)=>{
     conn.query("select * from medicines",(err, rows, fields)=>{
         if(!err){
             console.log('Medicines JSON is here')
-            // res.send("<h1>"+rows[1].name+"</h1>")
             console.log(rows)
             res.send(rows)
         }
