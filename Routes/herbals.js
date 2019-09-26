887a04c2bd854e45a896a35b6c79f9e24adde98a
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-09-26 17:04:26 +0800

    added the route for fetching herbal meds in database

---
+++ Routes/herbals.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const conn = require('../connection')
+const Router = express.Router()
+
+Router.get("/",(req, res)=>{
+const qry = "SELECT * FROM medicines where category = 'herbal' "
+    
+conn.query(qry,(err, rows, fields)=>{
+        if(!err){
+            console.log("fetched rows")
+            console.log(rows)
+            res.send(rows)
+            
+        }
+        else{
+            console.log("Failed to search")
+            console.log(err)
+        }
+    })
+})
+
+module.exports = Router
