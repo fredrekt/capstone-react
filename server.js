3bdbd63269d7b5124ce186835a0d3c023e172566
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-10-04 03:37:45 +0800

    added the session and the routes for the ratings and the meds/item

--- server.js
+++ server.js
@@ -9,10 +9,36 @@ const searchMeds = require('./Routes/searchMeds')
 const herbalMeds = require('./Routes/herbals')
 const users = require('./Routes/users')
 const login = require('./Routes/login')
+const medsitem = require('./Routes/medicineitem')
+const session = require('express-session'); 
+
+//ratings 
+const addrating = require('./Routes/Ratings/addrating')
 
 var app = express()
 // app.use(bodyParser.urlencoded({ extended: false}))
 // app.use(bodyParser.json())
+
+//session declaration
+const {
+    node_env = 'development',
+    session_name = 'sid',
+    session_secret = 'sessions-secret'
+} = process.env;
+//use session 
+app.use(session({
+    name: session_name,
+    resave: false,
+    saveUninitialized: false,
+    secret: session_secret,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        secure: node_env === 'production',
+        httpOnly: false
+    },
+
+}));
+
 //first medicine fetch from database to server sa web browser storing it to json
 app.use("/meds", prodRoute)
 // app.use("/meds2",prodRoute)
@@ -37,5 +63,12 @@ app.use("/users",users)
 
 //login route
 app.use("/login",login)
+
+//meds item
+app.use('/medicines', medsitem);
+
+//Rating Routes
+
+app.use('/add-rating',addrating)
 
 app.listen(3001)
