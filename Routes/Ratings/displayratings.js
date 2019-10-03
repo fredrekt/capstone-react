b922574a6805af7f3ba48cffaa290a69b7732f8b
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-10-04 03:36:19 +0800

    added the rating routes

---
+++ Routes/Ratings/displayratings.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const conn = require('../../connection')
+const Router = express.Router()
+
+Router.use(express.json());
+Router.use(express.urlencoded({ extended: true }));
+
+Router.post('/', (req,res) =>{
+    const { username,password } = req.body;
+    const getUser = `SELECT * FROM users WHERE username='${username}' AND password='${password}'`;
+    conn.query(getUser, (err, results) =>{
+        if(err) throw err
+        req.session.username = results[0].username;
+        console.log('this is session:',req.session.username)
+        res.json(results).status(200);
+    });       
+});
+
+
+module.exports = Router
