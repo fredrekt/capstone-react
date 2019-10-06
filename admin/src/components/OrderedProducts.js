e159f577ba4620d134ea069ebf44cf27380836cc
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-10-06 15:15:55 +0800

    added the OrderedProducts Component

---
+++ admin/src/components/OrderedProducts.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { MDBContainer } from 'mdbreact'
+
+
+class OrderedProducts extends Component{
+    render(){
+        return(
+            <div>
+                <MDBContainer>
+                    <h1>Hello World Ordered</h1>
+                </MDBContainer>
+            </div>
+        )
+    }
+}
+export default OrderedProducts
