92d1e1fa7ebcbf28f1fd5b6ba96b8769618a687c
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-10-06 18:29:22 +0800

    added the ordered products components

--- admin/src/components/OrderedProducts.js
+++ admin/src/components/OrderedProducts.js
@@ -1,13 +1,70 @@
 import React, { Component } from 'react'
-import { MDBContainer } from 'mdbreact'
+import { MDBContainer, MDBTableHead, MDBTable, MDBTableBody, MDBBtn } from 'mdbreact'
 
 
 class OrderedProducts extends Component{
     render(){
         return(
-            <div>
+            <div style={{'margin-left':'15%'}}>
                 <MDBContainer>
-                    <h1>Hello World Ordered</h1>
+                <h1 className="admin-prod-h text-left">
+                     Ordered Inventory
+                </h1>
+                    <MDBTable hover>
+                            <MDBTableHead>
+                                <tr>
+                                <th>ORDER #</th>
+                                <th>USER #</th>
+                                <th>MED #</th>
+                                <th>QTY</th>
+                                <th>STATUS</th>
+                                </tr>
+                            </MDBTableHead>
+                            <MDBTableBody>
+                                <tr>
+                                <td>1</td>
+                                <td>Romer</td>
+                                <td>bio-2354</td>
+                                <td>20</td>
+                                <td>
+                                    <MDBBtn
+                                    color="success"
+                                    size="sm"
+                                    >
+                                        Approve
+                                    </MDBBtn>
+                                </td>
+                                </tr>
+                                <tr>
+                                <td>2</td>
+                                <td>Fred</td>
+                                <td>neos-0921</td>
+                                <td>15</td>
+                                <td> 
+                                    <MDBBtn
+                                    color="success"
+                                    size="sm"
+                                    >
+                                        Approve
+                                    </MDBBtn>
+                                </td>
+                                </tr>
+                                <tr>
+                                <td>3</td>
+                                <td>Clydde</td>
+                                <td>alax-0982</td>
+                                <td>100</td>
+                                <td>
+                                    <MDBBtn
+                                    color="success"
+                                    size="sm"
+                                    >
+                                        Approve
+                                    </MDBBtn>
+                                </td>
+                                </tr>
+                            </MDBTableBody>
+                            </MDBTable>
                 </MDBContainer>
             </div>
         )
