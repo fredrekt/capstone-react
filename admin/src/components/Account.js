238eb5f77cfdaab48f2ab8ebc889c7c8d0ed5dcd
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-10-06 18:24:02 +0800

    added account edit for admin

---
+++ admin/src/components/Account.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Account extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello Account Settings</h1>
+            </div>
+        )
+    }
+}
+export default Account
