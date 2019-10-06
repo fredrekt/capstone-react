99857d38e5bac8fa4542abac48f78e1c8301190c
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-10-06 15:16:34 +0800

    updated the routes

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,18 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import SideNav from './SideNav';
 import NavigationBar from './Navbar'
-
+import Home from './components/Home'
 //BrowserRouter, Routes & Switch
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import AddProducts from './AddProducts';
 import Login from './components/Login';
+import OrderedProducts from './components/OrderedProducts';
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
           <SideNav/>
           <Switch>
-            <Route exact path="/add-products" component={AddProducts} />
+            <Route exact path="/" component={Home} />
+            <Route path="/add-products" component={AddProducts} />
+            <Route path="/ordered-products" component={OrderedProducts} />
             <Route path="/login" component={Login}/>
           </Switch>
       </div>
