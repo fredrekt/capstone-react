82a4d32d1cf32178d1840e696ebe008a7d61b4b5
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-10-10 22:33:40 +0800

    added the Home Component

--- admin/src/components/Home.js
+++ admin/src/components/Home.js
@@ -4,7 +4,20 @@ class Home extends Component{
     render(){
         return(
             <div>
-                <h1>Hello World Home</h1>
+                <h1 className="p-3">Welcome Admin!</h1>
+                    <p className="grey-text">
+                        You are entitled to these admin priviledges.  
+                    </p>
+                    <h4 className="p-3">
+                        You control everything
+                    </h4>
+                    <ul>
+                        <li>You can add products</li>
+                        <li>You can approve orders</li>
+                        <li>You can track orders</li>
+                        <li>You can update the stocks</li>
+                        <li>You can see whatever the hell's going on with the client side of the application</li>
+                    </ul>
             </div>
         )
     }
