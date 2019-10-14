ede166db3823169d45ed6fa3a7e2611a7be9c2ac
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-10-14 17:55:33 +0800

    updated some stuff and add an arrow function to fetch the update stock in medicines table

--- capstone-react/src/components/Cart.js
+++ capstone-react/src/components/Cart.js
@@ -1,183 +1,3 @@
-// import React, { Component } from 'react'
-// import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBCard, MDBCardText, MDBCardBody, MDBCardTitle, MDBCardImage, MDBIcon, MDBAlert, MDBAnimation } from 'mdbreact' 
-// import Breadcrumb from './Breadcrumb'
-// import biogesic from '../img/meds-bio.jpeg'
-// import biogesic2 from '../img/meds-sample-pic-2.jpeg'
-// import biogesic3 from '../img/meds-sample-pic-3.jpeg'
-// import biogesic4 from '../img/meds-sample-pic-4.jpeg'
-// import phlogo from '../img/ph-flag.png'
-// import ScrollButton from './ScrollButton'
-
-// class Cart extends Component{
-//     render(){
-//         return(
-//             <div>
-//                 <Breadcrumb bheader="Shopping Cart" bcurrent="Cart" />
-//                     <div className="cart-container"> 
-//                         <MDBAnimation type="slideInUp">  
-//                             <MDBContainer>
-//                                 <MDBAlert color="success" dismiss>
-//                                 <strong>This is your cart!</strong> The items that wished to buy are all here, last final step to obtain items is payment.
-//                                 </MDBAlert>
-//                             </MDBContainer>
-//                         </MDBAnimation>
-//                         <MDBContainer>
-//                             <h1 className="cart-header">
-//                                 Your Cart
-//                             </h1>
-//                             <p className="cart-sub-h grey-text">
-//                                 You have 3 iems in your cart
-//                             </p>
-//                             <MDBRow>
-//                                 <MDBCol>
-//                                     <div className="cart-item-container">
-//                                         <img className="cart-img rounded" src={biogesic}/>
-//                                         <h4 className="cart-item-name">Neosep</h4>
-//                                         <p className="cart-item-id grey-text">ID: 0002390</p>
-//                                     </div>
-//                                 </MDBCol>
-//                                 <MDBCol style={{'text-align':'right'}}>
-//                                     <h2 className="cart-price"> <span>&#8369;</span> 75.00</h2>
-//                                     <p className="qty-sub-txt grey-text">Quantity: 1</p>
-//                                     <MDBBtn style={{'margin-top':'-2%'}} outline color="danger" size="sm">Remove</MDBBtn>
-//                                 </MDBCol>
-//                             </MDBRow>
-//                             <div className="cart2-container">
-//                             <MDBRow>
-//                                 <MDBCol>
-//                                     <div className="cart-item-container">
-//                                         <img className="cart-img rounded" src={biogesic2}/>
-//                                         <h4 className="cart-item-name">Biogesic</h4>
-//                                         <p className="cart-item-id grey-text">ID: 01231251</p>
-//                                     </div>
-//                                 </MDBCol>
-//                                 <MDBCol style={{'text-align':'right'}}>
-//                                     <h2 className="cart-price"> <span>&#8369;</span> 23.00</h2>
-//                                     <p className="qty-sub-txt grey-text">Quantity: 1</p>
-//                                     <MDBBtn style={{'margin-top':'-2%'}} outline color="danger" size="sm">Remove</MDBBtn>
-//                                 </MDBCol>
-//                             </MDBRow>
-//                             </div>
-//                             <MDBRow>
-//                                 <MDBCol>
-//                                     <div className="cart-item-container">
-//                                         <img className="cart-img rounded" src={biogesic}/>
-//                                         <h4 className="cart-item-name">Paracetamol Acidreflux</h4>
-//                                         <p className="cart-item-id grey-text">ID: 0001510</p>
-//                                     </div>
-//                                 </MDBCol>
-//                                 <MDBCol style={{'text-align':'right'}}>
-//                                     <h2 className="cart-price"> <span>&#8369;</span> 13.00</h2>
-//                                     <p className="qty-sub-txt grey-text">Quantity: 1</p>
-//                                     <MDBBtn style={{'margin-top':'-2%'}} outline color="danger" size="sm">Remove</MDBBtn>
-//                                 </MDBCol>
-//                             </MDBRow>
-//                             <MDBRow>
-//                                 <MDBCol>
-                                    
-//                                 </MDBCol>
-//                                 <MDBCol style={{'text-align':'right'}}>
-//                                     <h3 className="cart-total-txt">Order-Sub-Total &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span>&#8369;</span> 168</h3>
-//                                     <p className="cart-items-left grey-text">3 items</p>
-//                                     <MDBBtn style={{'width':'400px'}} size="md" href="/payment-method" color="success"><h5 className="cart-checkout-txt" >Checkout</h5></MDBBtn>
-//                                 </MDBCol>
-//                             </MDBRow>
-//                         </MDBContainer>
-                        
-//                         <div className="cart-suggestions-container">
-//                             <MDBContainer>
-//                                 <h4 className="cart-suggestions-h grey-text">
-//                                     Suggested Medicines
-//                                 </h4>
-//                                 <MDBRow>
-//                                     <MDBCol>
-//                                     <a className="meds-card-link" href="/meds/item">
-//                                                     {/* <MDBView hover zoom> */}
-//                                                         <MDBCard>
-//                                                             <MDBCardImage hover zoom className="img-fluid" src={biogesic} waves />
-//                                                             <MDBCardBody>
-//                                                             <MDBCardTitle><h3 className="med-item-header">Biogesic</h3></MDBCardTitle>
-//                                                             <MDBCardText>
-//                                                                 <div className="med-content-container">
-//                                                                     <div className="grey-text text-center">
-//                                                                     <p className="med-sub-h">
-//                                                                         <MDBIcon icon="tags" />
-//                                                                             Biogesic
-//                                                                         </p>
-//                                                                     </div>
-//                                                                     <div className="prizeandlogo-container">
-//                                                                         <div className="black-text text-center">
-//                                                                             <h4 className="med-price"><img src={phlogo} className="icon-ph-flag" alt=""/> <span>&#8369;</span> 6.00</h4> 
-//                                                                         </div>    
-//                                                                     </div>
-//                                                                 </div>
-//                                                             </MDBCardText>
-//                                                             </MDBCardBody>
-//                                                         </MDBCard>
-//                                                         {/* </MDBView> */}
-//                                                         </a>
-//                                     </MDBCol>
-//                                     <MDBCol>
-//                                     <a className="meds-card-link" href="/meds/item">
-//                                                         <MDBCard>
-//                                                             <MDBCardImage hover zoom className="img-fluid" src={biogesic2} waves />
-//                                                             <MDBCardBody>
-//                                                             <MDBCardTitle><h3 className="med-item-header">Enervon</h3></MDBCardTitle>
-//                                                             <MDBCardText>
-//                                                                 <div className="med-content-container">
-//                                                                     <div className="grey-text text-center">
-//                                                                     <p className="med-sub-h">
-//                                                                         <MDBIcon icon="tags" />
-//                                                                             Enervon
-//                                                                         </p>
-//                                                                     </div>
-//                                                                     <div className="prizeandlogo-container">
-//                                                                         <div className="black-text text-center">
-//                                                                             <h4 className="med-price"><img src={phlogo} className="icon-ph-flag" alt=""/> <span>&#8369;</span> 5.00</h4> 
-//                                                                         </div>    
-//                                                                     </div>
-//                                                                 </div>
-//                                                             </MDBCardText>
-//                                                             </MDBCardBody>
-//                                                         </MDBCard>
-//                                                         </a>
-//                                     </MDBCol>
-//                                     <MDBCol>
-//                                     <a className="meds-card-link" href="/meds/item">
-//                                                         <MDBCard>
-//                                                             <MDBCardImage hover zoom className="img-fluid" src={biogesic4} waves />
-//                                                             <MDBCardBody>
-//                                                             <MDBCardTitle><h3 className="med-item-header">Alaxan</h3></MDBCardTitle>
-//                                                             <MDBCardText>
-//                                                                 <div className="med-content-container">
-//                                                                     <div className="grey-text text-center">
-//                                                                     <p className="med-sub-h">
-//                                                                         <MDBIcon icon="tags" />
-//                                                                             Alaxan Fr
-//                                                                         </p>
-//                                                                     </div>
-//                                                                     <div className="prizeandlogo-container">
-//                                                                         <div className="black-text text-center">
-//                                                                             <h4 className="med-price"><img src={phlogo} className="icon-ph-flag" alt=""/> <span>&#8369;</span> 5.00</h4> 
-//                                                                         </div>    
-//                                                                     </div>
-//                                                                 </div>
-//                                                             </MDBCardText>
-//                                                             </MDBCardBody>
-//                                                         </MDBCard>
-//                                                         </a>
-//                                     </MDBCol>
-//                                 </MDBRow>
-//                             </MDBContainer> 
-//                         </div>
-//                     </div>
-//                     <ScrollButton/>
-//             </div>
-//         )
-//     }
-// }
-// export default Cart
-
 import React, { Component } from 'react'
 import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBCard, MDBCardText, MDBCardBody, MDBCardTitle, MDBCardImage, MDBIcon, MDBAlert, MDBAnimation } from 'mdbreact' 
 import Breadcrumb from './Breadcrumb'
@@ -188,6 +8,10 @@ import biogesic4 from '../img/meds-sample-pic-4.jpeg'
 import phlogo from '../img/ph-flag.png'
 import ScrollButton from './ScrollButton'
 
+//Toastify 
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 class Cart extends Component{
     constructor(props){
         super(props)
@@ -196,7 +20,9 @@ class Cart extends Component{
             usersLogin: null,
             cart: [],
             selectedCart: [],
-            totalprice: null
+            totalprice: null,
+            cartitems: '',
+            checkoutname: 'Checkout'
           }
         this.med_key = props;
         }
@@ -240,11 +66,51 @@ class Cart extends Component{
             });
         }
 
+        notifySuccess = () => {
+            toast.success(
+            <div>
+            <MDBIcon size='lg' icon="check-circle" />
+            <span className="success-h"> Checkout Successful!</span></div>, {
+                position: toast.POSITION.TOP_RIGHT,
+                autoClose:1200
+              });
+        }
+        anotherfunct = () =>{
+            this.setState({
+                checkoutname: <div class="spinner-border white-text" role="status">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+            })
+            setTimeout(()=>{
+                this.notifySuccess()
+            },1000)
+            setTimeout(()=>{
+                this.setState({checkoutname:'Checkout'})
+            },1000)
+        }
+
+        checkout = () =>{
+            fetch(`/cart/updateCart`)
+            .then(res => res.json())
+            .then(data =>{
+                console.log('Success!',data)
+                
+            },
+            this.anotherfunct()
+            )
+            
+            
+            .catch(error =>{
+                console.log("Failed",error)
+            })
+        }
+
     render(){
         const cart = this.state.cart;
         return(
             <div>
                 <Breadcrumb bheader="Shopping Cart" bcurrent="Cart" />
+                    <ToastContainer/>
                     <div className="cart-container"> 
                         <MDBAnimation type="slideInUp">  
                             <MDBContainer>
@@ -262,6 +128,7 @@ class Cart extends Component{
                             <p className="cart-sub-h grey-text">
                                 You have {cart.length} items in your cart
                             </p>
+                            {this.state.cartitems}
                             {cart.map(item => 
                                 <MDBRow>
                                 <MDBCol>
@@ -288,7 +155,17 @@ class Cart extends Component{
                                 <MDBCol style={{'text-align':'right'}}>
                                     <h3 className="cart-total-txt">Order-Sub-Total &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span>&#8369;</span> {this.state.totalprice}</h3>
                                     <p className="cart-items-left grey-text">{cart.length} items</p>
-                                    <MDBBtn style={{'width':'400px'}} size="md" color="success"><h5 className="cart-checkout-txt">Checkout</h5></MDBBtn>
+                                    <MDBBtn
+                                    style={{'width':'400px'}} 
+                                    size="md" 
+                                    color="success"
+                                    onClick={this.checkout}
+                                    >
+                                    <h5 
+                                    className="cart-checkout-txt">
+                                    {this.state.checkoutname}
+                                    </h5>
+                                    </MDBBtn>
                                 </MDBCol>
                             </MDBRow>
                         </MDBContainer>
